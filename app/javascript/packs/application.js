1ec187bda6c7b6b43b1a1853a711135b95850fee
Leon <kliwaru@gmail.com>
2019-11-13 16:42:02 +0800

    jquery added to project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 require("custom/method_1")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -19,9 +20,13 @@ console.log('application packs loaded');
 document.addEventListener('turbolinks:load', () => {  
     console.log('turbolinks loaded')
 
-    document.getElementById("custombutton").addEventListener("click", () => {
-        console.log('From method_2 pack/app.js');
-        document.getElementsByTagName('h1')[0].innerText = "From method_2 pack/app.js"
+    // document.getElementById("custombutton").addEventListener("click", () => {
+    //     console.log('From method_2 pack/app.js');
+    //     document.getElementsByTagName('h1')[0].innerText = "From method_2 pack/app.js"
+    // })
+
+    $('#custombutton').on('click', () => {
+        alert('helo from jquery of application.js')
     })
 
 })
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+
+
+10.times do 
+    lib = Library.new(
+        title: Faker::TvShows::StrangerThings.character,
+        description: Faker::TvShows::StrangerThings.quote,
+    )
+    p "Entry saved." if lib.save
+end
